a68b77ff0dc8ac8b93563703cf7b0653d5e5edd4
Kunaaal <kunaaal.rao@gmail.com>
2026-03-15 15:54:28 +0530

    add cn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { cn } from '../utils/cn';
+</script>
+
+<div class={cn('flex h-screen w-full flex-col items-center justify-center', 'aspect-auto')}>
+	<h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
+	<p class="text-2xl">
+		Visit <a href="https://svelte.dev/docs/kit" class="text-blue-500">svelte.dev/docs/kit</a> to read
+		the documentation
+	</p>
+</div>
